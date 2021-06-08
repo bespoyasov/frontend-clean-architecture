7514b8c80ef2a77d5e54141fe29db622592c1b0e
Alexander Bespoyasov <forjob159@gmail.com>
2021-06-08 17:18:27 +0300

    Move `Ingredient` type to domain

---
+++ src/domain/ingredient.d.ts
@@ -0,0 +1,1 @@
+type Ingredient = "chocolate" | "peanuts" | "cocoa" | "marshmallow" | "cherry";
--- src/shared-kernel.d.ts
+++ src/shared-kernel.d.ts
@@ -2,5 +2,3 @@ type Email = string;
 type UniqueId = string;
 type DateTimeString = string;
 type PriceCents = number;
-
-type Ingredient = "chocolate" | "peanuts" | "cocoa" | "marshmallow" | "cherry";
